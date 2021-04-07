c303d27733f27c71d9140397404943fbc635eaa5
Yatharth <yatharthverma070@gmail.com>
2021-04-07 18:40:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Videos</h1>
       </header>
     </div>
   );
